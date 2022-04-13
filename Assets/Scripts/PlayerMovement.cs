5316b87ce3d8c2240c4adf1dbc0eadbdff5c7cdc
tserlis <erlist@tcd.ie>
2022-04-13 19:54:24 +0100

    tightened Mantle, no longer continues jumping velocity post mantling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    private enum State {
+        STANDING,
+        MANTLING,
+    }
+
+#region Object Components
     public CharacterController controller;
     public Transform groundCheck;
     public Transform mantlePoint;
 
+#endregion
+
+
+#region Fields
     public float speed = 10f;
     public float gravity = -9.81f;
     public float jumpHeight = 10f;
@@ -17,62 +27,82 @@ public class PlayerMovement : MonoBehaviour
     public float mantleRange = 1.25f;
     public LayerMask groundMask; 
 
+    private State state;
+
     private Vector3 velocity;
     private bool isGrounded;
+
+    private int layerMask = 1 << 8;
+    
+
+#endregion
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.STANDING;
+        layerMask = ~layerMask;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        switch(state) {
+            case State.STANDING:
+                #region Movement
+                isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        //Add Jump height
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        //Mantle Ability
-        if (Input.GetButton("Jump") && !isGrounded) {
-            //Fire Ray In Front of Player
-            int layerMask = 1 << 8;
-            layerMask = ~layerMask;
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, mantleRange, layerMask)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log("Mantle Ray Hit!");
-                if (hit.collider.bounds.Contains(mantlePoint.position) == false) {
-                    Debug.Log("Mantling"); 
-                    controller.enabled = false;
-                    Vector3 mantleMove = transform.up * mantlePoint.position.y + transform.forward * (mantlePoint.position.z+1);
-                    transform.Translate(mantleMove, Space.World);
-                    controller.enabled = true;
+                if (isGrounded && velocity.y < 0)
+                {
+                    velocity.y = -2f;
                 }
-            }
-            else {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * mantleRange, Color.red);
-                Debug.Log("Mantle Ray Miss");
-            }
+                
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
+
+                Vector3 move = transform.right * x + transform.forward * z;
+
+                controller.Move(move * speed * Time.deltaTime);
+
+                //Add Jump height
+                if (Input.GetButtonDown("Jump") && isGrounded) {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                }
+
+                
+
+                velocity.y += gravity * Time.deltaTime;
+
+                controller.Move(velocity * Time.deltaTime);
+                #endregion 
+
+                #region Transition to Mantling
+                if (Input.GetButton("Jump") && !isGrounded) {
+                    RaycastHit hit;
+                    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, mantleRange, layerMask)) {
+                        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                        Debug.Log("Mantle Ray Hit!");
+                        if (hit.collider.bounds.Contains(mantlePoint.position) == false) {
+                            state = State.MANTLING;
+                        }
+                    }
+                    else {
+                        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * mantleRange, Color.red);
+                        Debug.Log("Mantle Ray Miss");
+                    }
+                }
+                #endregion
+                break;
+
+            case State.MANTLING:
+                Debug.Log("Mantling"); 
+                controller.enabled = false;
+                Vector3 mantleMove = new Vector3(0, 4, 2);
+                transform.Translate(mantleMove);
+                controller.enabled = true;
+                velocity.y = -2f;       //have to reset velocity or else jumping velocity will still be applied post-mantle
+                state = State.STANDING;
+                break;
         }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
     }
 }
